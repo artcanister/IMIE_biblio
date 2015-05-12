850fa12d07db18ecd219d1b542638b574367eb2b
Charlie Lucas <charlielucas.cl@gmail.com>
2015-05-12 14:58:57 +0200

    add form to create a book

--- src/AppBundle/Controller/BookController.php
+++ src/AppBundle/Controller/BookController.php
@@ -6,6 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use AppBundle\Entity\Book;
+use Symfony\Component\HttpFoundation\Request;
+
 /**
  * @Route("/books")
  *
@@ -16,10 +19,27 @@ class BookController extends Controller {
      *@Template
      */
     public function listAction(){
-        //-action : config externe
         $books = $this->getDoctrine()->getRepository('AppBundle:Book')->findAll();
         
         return array('books' => $books);
+    }
+    /**
+     *@Route("/add", name="books_add")
+     *@Template
+     */
+    public function addAction(Request $request){
+
+        $book = new Book();
+        $form = $this->createForm('appbundle_book', $book);
+        $form->handleRequest($request);
+        if($form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($book);
+            $em->flush();
+        }
+
+        
+        return array('form' => $form->createView());
     }
 }
 
