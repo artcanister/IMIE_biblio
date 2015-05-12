b303b988f8e56fa64741d3e5c3b3c98a95bf3e3d
lactor <lactor.galaway@gmail.com>
2015-05-12 12:22:38 +0200

    commit before merge

--- src/AppBundle/Controller/BookRestController.php
+++ src/AppBundle/Controller/BookRestController.php
@@ -5,16 +5,19 @@ use FOS\RestBundle\Controller\Annotations\View;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use AppBundle\Entity\Book;
 
 class BookRestController extends Controller {
     /**
      * 
      * @ApiDoc(
      *  resource = true,
-     *  description="Return all our books",
+     *  description="Return all our books (get)",
      *  tags={
      *      "in-development"}
      * )
+     * @return array
+     * 
      */
     public function getBooksAction(){
         //-action : config externe
@@ -27,7 +30,7 @@ class BookRestController extends Controller {
      * 
      * @ApiDoc(
      *  resource = true,
-     *  description="Get a book with his id"
+     *  description="Get a book with his id",
      *  tags={
      *      "in-development"}
      * )
@@ -38,8 +41,48 @@ class BookRestController extends Controller {
         return $book;
     }
     
-    //public function addBookAction($author, $releasedate, $title, $categories, $description)
+    /**
+     * @ApiDoc(
+     *  resource= true,
+     *  description="edit a book and persist in database",
+     *  tags={
+     *      "in-development"}
+     * )
+     * 
+     * @ParamConverter("post", class="AppBundle:Book")
+     */
+    public function putBookAction(){
+        //is a complete edit of a book and saving method
+        
+        
+    }
     
-    
+    /**
+     * @ApiDoc(
+     *  resource= true,
+     *  description="This method delete a book. All its fields, but only one resource.",
+     *  tags={
+     *      "in-development"}
+     * )
+     * 
+     * @ParamConverter("post", class="AppBundle:Book")
+     */
+   public function deleteBookAction(Book $book){
+       $em = $this->getDoctrine()->getManager();
+       $em->remove($book);
+   }
+   
+   /**
+     * @ApiDoc(
+     *  resource= true,
+     *  description="You can use it to update the title, author, categories and all the informations of the book.",
+     *  tags={
+     *      "in-development"}
+     * )
+    * @ParamConverter("post", class="AppBundle:Book")
+    */
+   public function updateBookAction(Book $book){
+       
+   }
 }
 
